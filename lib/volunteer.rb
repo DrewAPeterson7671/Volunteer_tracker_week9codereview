763e2c6215523472c58ecaa71ae50c65c4f67af8
Student <example@example.com>
2020-01-10 13:44:10 -0800

    meaningfully fail save method, which has to be done before second test of .all can pass

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -28,4 +28,9 @@ class Volunteer
     self.get_volunteers('SELECT * FROM volunteers;')
   end
 
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id})")
+    @id = result.first().fetch('id').to_i
+  end
+
 end
