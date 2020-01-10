6166ed566f5a39dcb1cc8c07f5263870ffdc92bb
Student <example@example.com>
2020-01-10 13:27:38 -0800

    Pass the first volunteer test of retrieving name

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -3,7 +3,7 @@ class Volunteer
   attr_accessor :name, :project_id
 
   def initialize(attributes)
-    @name = attributes.fetch(:name1)
+    @name = attributes.fetch(:name)
     @project_id = attributes.fetch(:project_id)
     @id = attributes.fetch(:id)
   end
